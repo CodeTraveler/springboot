263c8f3ec91d6865b51668e938536dd69a564fed
CodeTraveler <puneamdo@gmail.com>
2019-06-02 06:13:41 -0700

    commit from arp

--- springBootDemo/src/main/java/com/clc/java/service/ServletInitializer.java
+++ springBootDemo/src/main/java/com/clc/java/service/ServletInitializer.java
@@ -5,9 +5,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("edited by other devloper");
 		return application.sources(SpringBootDemoApplication.class);
 	}
 
