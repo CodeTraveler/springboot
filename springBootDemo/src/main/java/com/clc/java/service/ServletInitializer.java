2aaadb28e4dcb1a4fc8d81005a69c49de0593f95
CodeTraveler <30338796+CodeTraveler@users.noreply.github.com>
2019-06-02 06:01:01 -0700

    Update ServletInitializer.java
    insert syso

--- springBootDemo/src/main/java/com/clc/java/service/ServletInitializer.java
+++ springBootDemo/src/main/java/com/clc/java/service/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("edited by other devloper");
 		return application.sources(SpringBootDemoApplication.class);
 	}
 
